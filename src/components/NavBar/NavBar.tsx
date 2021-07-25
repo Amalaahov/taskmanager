ad0ce452c05eecdb5a454a0ec112b2fc4bb78e85
whoami <whoami@192.168.1.188>
2021-07-25 20:31:05 +0300

    pre-release v.3

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,11 +7,11 @@ import logo from './../../assets/logo.png'
 const Navbar = () => {
     return (
         <div className={classes.Navbar}>
-            <img src={logo} alt='logo'/>
+            <img src={logo} alt='logo' />
             <div className={classes.stickyLink}>
-                <div className={classes.linkdiv}><NavLink activeClassName={classes.activeLink} className={classes.links} to="/mainpage"><img src={dashboard} alt='dashboard'/> Dashboard</NavLink></div>
-                <div className={classes.linkdiv}><NavLink activeClassName={classes.activeLink} className={classes.links} to="/addtask"><img src={addtask} alt='dashboard'/> Add task</NavLink></div>
-            </div>           
+                <div className={classes.linkdiv}><NavLink activeClassName={classes.activeLink} className={classes.links} to="/mainpage"><img src={dashboard} alt='dashboard' /> Dashboard</NavLink></div>
+                <div className={classes.linkdiv}><NavLink activeClassName={classes.activeLink} className={classes.links} to="/addtask"><img src={addtask} alt='dashboard' /> Add task</NavLink></div>
+            </div>
         </div>
     )
 }
